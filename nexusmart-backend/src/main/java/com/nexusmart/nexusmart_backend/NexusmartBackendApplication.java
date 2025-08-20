4977b1336a11018ad735186d9b6b5e21a72da792
Abhilash <abhiabhilas1456910@gamil.com>
2025-08-20 14:05:56 +0530

    Clean project structure: add frontend and backend

---
+++ nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/NexusmartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nexusmart.nexusmart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusmartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusmartBackendApplication.class, args);
+	}
+
+}
