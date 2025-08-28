5f8bdcb8ff9ff34441b23ff0b0af641676259575
abhi-codespace <one.abhi@outlook.com>
2025-08-28 13:33:56 +0530

    feat(cart): add Cart REST API and connect to service endpoint

--- nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/entity/Cart.java
+++ nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/entity/Cart.java
@@ -1,6 +1,36 @@
 package com.nexusmart.nexusmart_backend.entity;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="cart")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name ="user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL)
+    private List<CartItem> items=new ArrayList<>();
 }
--- nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/serviceInterface/CartService.java
+++ nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/serviceInterface/CartService.java
@@ -1,5 +1,18 @@
 package com.nexusmart.nexusmart_backend.serviceInterface;
 
+import java.util.List;
+
+import com.nexusmart.nexusmart_backend.entity.Cart;
+
 public interface CartService {
 
+    List<Cart> getAllCarts();
+
+    Cart getCartById(Long id);
+
+    Cart createCart(Cart cart);
+
+    Cart updateCart(Long id,Cart cart);
+
+    void deleteCart(Long id);
 }
