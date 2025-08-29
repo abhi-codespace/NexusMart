e3d6d494705e9b16d1b6da885c7bb6ab45ca969e
abhi-codespace <one.abhi@outlook.com>
2025-08-29 23:29:45 +0530

    feat(RoleRepository): adding new operation to perform admin

--- nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/repository/RoleRepository.java
+++ nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/repository/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.nexusmart.nexusmart_backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nexusmart.nexusmart_backend.entity.Role;
+import com.nexusmart.nexusmart_backend.entity.RoleType;
 
 public interface RoleRepository  extends JpaRepository<Role,Long>{
-    
+     Optional<Role> findByName(RoleType name);
 }
