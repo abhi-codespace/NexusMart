d9eb3b7efe9fc745772d1e745ac777933e23a344
abhi-codespace <one.abhi@outlook.com>
2025-08-30 01:09:39 +0530

    Merge feature1 UserRepository changes

--- nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/repository/UserRepository.java
+++ nexusmart-backend/src/main/java/com/nexusmart/nexusmart_backend/repository/UserRepository.java
@@ -9,5 +9,8 @@ import com.nexusmart.nexusmart_backend.entity.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
     Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
     boolean existsByEmail(String email);  
 }
